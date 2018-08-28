696472c58d12b8dcbc503fa50a699cb6186fbc0c
darkmarthur <darkmarthur@gmail.com>
2018-08-28 01:09:10 -0500

    pagesoup

--- python/hello.py
+++ python/hello.py
@@ -1,6 +1,9 @@
-print(123 + 123)
-msg = "Hello World"
-print(msg)
-msg
-msg.capitalize()
-msg.split()
+import bs4
+from urllib.request import urlopen as ureq
+from bs4 import BeautifulSoup as soup
+my_url = "https://www.newegg.com/global/mx/Gaming-Laptops/SubCategory/ID-3365?Tid=899891"
+uClient = ureq(my_url)
+pageHtml = uClient.read()
+uClient.close()
+pageSoup = soup(pageHtml, "html.parser")
+pageSoup.h1
