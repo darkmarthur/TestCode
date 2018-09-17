70d8abd36dc0672ee3f1d0f5b4a8a0f032e26351
darkmarthur <darkmarthur@gmail.com>
2018-09-16 22:07:06 -0500

    python stuy

--- python/hello.py
+++ python/hello.py
@@ -6,5 +6,35 @@ uClient = ureq(my_url)
 pageHtml = uClient.read()
 uClient.close()
 ps = soup(pageHtml, "html.parser")
-ps.h1
+# print(ps.body.prettify())
+#  ps.h1
+
+# class="item-container  "
+# class="items-view is-grid"
+# class="item-title"
+# price-currency-label strong sup
+# item-brand a
+
+
 containers = ps.findAll("div", {"class" : "item-container"})
+# len(containers)
+# item = containers[0]
+    # itemShipping = item.findAll("li", {"class":"price-ship"})[0].text.strip()
+    # print("itemShipping: " + itemShipping)
+
+
+fileName = "products.csv"
+f = open(fileName, "w")
+headers = "brand, price, title\n"
+f.write(headers)
+
+for item in containers:
+    itemBrand = item.div.a.img["title"]
+    itemPrice = item.findAll("li", {"class":"price-current"})[0].strong.text
+    itemTitle = item.findAll("a", {"class":"item-title"})[0].text
+    # print("itemBrand: " + itemBrand)
+    # print("itemPrice: " + itemPrice)
+    # print("itemTitle: " + itemTitle)
+    f.write(itemBrand + ", " + itemPrice + ", " + itemTitle.replace(",", "|") + "\n")
+
+f.close()
